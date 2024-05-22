98dc62f2a381df07182055fc9a6af611e3f77357
jfernando02 <82566846+jfernando02@users.noreply.github.com>
2024-05-22 14:31:02 +1000

    Add initial ROS, pat

---
+++ 3806ict_assignment_3/src/communal_defines.cpp
@@ -0,0 +1,16 @@
+#define VISITED -1
+#define EMPTY 0
+#define SUB 1
+#define SURVIVOR 3
+
+#define SUB_START_X 0
+#define SUB_START_Y 0
+
+#define BOARD_H 8
+#define BOARD_W 8
+
+#define SUB_CAP 20
+#define SURVIVOR_COUNT 5
+#define SUB_COUNT 2
+
+#define SURVIVOR_DETECTION_RANGE 1
